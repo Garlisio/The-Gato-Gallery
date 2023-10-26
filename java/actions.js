8169d834020cc1985465f878327acaadd0f988c2
Garlisio <diego14fero@gmail.com>
2023-10-26 20:45:12 +0100

    The Gato Gallery

--- java/actions.js
+++ java/actions.js
@@ -1,25 +1,25 @@
 function initialize() {
 
-    const headerMenuIconOpen = document.getElementById("icon-open");
-    headerMenuIconOpen.addEventListener("click", showSideMenu);
-    const headerMenuIconClose = document.getElementById("icon-close");
-    headerMenuIconClose.addEventListener("click", closeSideMenu);
+    const header_menu_icon_open = document.getElementById("icon-open");
+    header_menu_icon_open.addEventListener("click", showSideMenu);
+    const header_menu_icon_close = document.getElementById("icon-close");
+    header_menu_icon_close.addEventListener("click", closeSideMenu);
 
 }
 
 function showSideMenu() {
 
-    const sideMenu = document.getElementById("side-menu");
-    sideMenu.classList.add("side-menu-open");
-    sideMenu.classList.remove("side-menu-close");
+    const side_menu = document.getElementById("side-menu");
+    side_menu.classList.add("side-menu-open");
+    side_menu.classList.remove("side-menu-close");
 
 }
 
 function closeSideMenu() {
 
-    const sideMenu = document.getElementById("side-menu");
-    sideMenu.classList.add("side-menu-close");
-    sideMenu.classList.remove("side-menu-open");
+    const side_menu = document.getElementById("side-menu");
+    side_menu.classList.add("side-menu-close");
+    side_menu.classList.remove("side-menu-open");
 
 }
 
